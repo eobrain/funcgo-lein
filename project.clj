c97cf413a79389ec132e2aa8b607546ecc5ef441
Eamonn O'Brien-Strain <eob@well.com>
2014-05-31 22:16:11 -0700

    Release 0.2.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.eamonn.funcgo/funcgo-lein-plugin "0.2.3"
+(defproject org.eamonn.funcgo/funcgo-lein-plugin "0.2.4"
   :description "Leiningen plugin to compile Functional Go to Clojure"
   :url "http://funcgo.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.eamonn.funcgo/funcgo-compiler "0.2.3"]]
+  :dependencies [[org.eamonn.funcgo/funcgo-compiler "0.2.4"]]
   :eval-in-leiningen true)
